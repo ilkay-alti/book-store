682e039a8299db3c95e94e24b75f731dc734adf0
ilkay Altınışık <ilkayalti@hotmail.com>
2023-07-23 18:01:06 +0300

    feat(projectRoot): Adding style pattern with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#0B7C6B",
+        primaryLight: "#E4FFFB",
+        secondary: "#FF6320",
+        secondaryLight: "#FFECE3",
+        statusBlue: "#219FFF",
+        statusBlueLight: "#E4F4FF",
+        statusGreen: "#17BD8D",
+        statusGreenLight: "#DFFEF5",
+        statusYellow: "#FFA114",
+        statusYellowLight: "#FFF1DC",
+        statusRed: "#FF4E3E",
+        statusRedLight: "#FFECEB",
       },
     },
   },
   plugins: [],
-}
+};
